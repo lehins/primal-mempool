17242db9562ce1ac234038947ac59a1133e55502
Alexey Kuleshevich <alexey@kuleshevi.ch>
2022-11-26 00:36:07 +0300

    Add test runtimes to hspec tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -3,9 +3,14 @@ module Main where
 import Spec
 import System.IO (BufferMode (LineBuffering), hSetBuffering, hSetEncoding, stdout, utf8)
 import Test.Hspec
+import Test.Hspec.Runner
 
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
   hSetEncoding stdout utf8
-  hspec spec
+  let config =
+        defaultConfig {
+        configTimes = True
+        }
+  hspecWith config spec
